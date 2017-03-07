c3002e66ed2c173c421a31c91c73ed18379e4b0a
Louis Bennette <louis.bennette@gmail.com>
2017-03-07 03:39:22 +0000

    Set up simple thread creation

--- FourFoursServer/FourFoursServer/FourFoursServer.py
+++ FourFoursServer/FourFoursServer/FourFoursServer.py
@@ -1,21 +1,32 @@
 # Echo server program
 import socket
+import threading 
+from threading import Thread
+import sqlite3
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50175              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.bind((HOST, PORT))
-
-  s.listen(1)
-  print("Listening on port ", PORT)
-  
-  conn, addr = s.accept()
+def parse_tcp_input(conn, addr):
+  print("Doing the good stuff")
   with conn:
-    print('Connected by', addr)
-    
-    while True:
+      print('Connected by', addr)
       data = conn.recv(1024)
       
-      if not data: break
+      if not data: return
 
       conn.sendall(b'Server answer is very generic')
+
+if __name__ == '__main__':
+
+  HOST = ''                 # Symbolic name meaning all available interfaces
+  PORT = 50175              # Arbitrary non-privileged port
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+
+    s.listen(1)
+    print("Listening on port ", PORT)
+    while True:
+      conn, addr = s.accept()
+      t = threading.Thread(target=parse_tcp_input, args=(conn, addr, ))
+      t.start()
+
+    
+
