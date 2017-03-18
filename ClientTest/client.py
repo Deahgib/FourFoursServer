9ccf0271b48fb46ed0e647d4598d5191cdca28f6
Louis Bennette <louis.bennette@gmail.com>
2017-03-18 20:57:37 +0000

    Added front end, website usinging PhP to display table contents

--- ClientTest/client.py
+++ ClientTest/client.py
@@ -2,14 +2,14 @@
 import socket
 import time
 
-HOST = 'localhost'    # The remote host
+HOST = 'www.bennette.co.uk'    # The remote host
 PORT = 50175              # The same port as used by the server
 
 for _ in range(1):
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		
 		s.connect((HOST, PORT))
-		s.sendall(bytes.fromhex('01 f2 83 e1 a3 44 da b7 af f4 d6 69 ca c9 01 00            63     00 00 00 02      0b      28 34 2f 34 29 2b 28 34 2f 34 29'))
+		s.sendall(bytes.fromhex('01 f2 83 e1 a3 44 da b7 af f4 d6 69 ca c9 01 00            63     00 00 00 0a      0b      28 34 2f 34 29 2b 28 34 2f 34 29'))
 		time.sleep(1)
 		
 		
---
+++ Website/Index.php
@@ -0,0 +1,38 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "TESTING"; ?></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+
+<?php
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+
+echo "£";
+
+echo "$";
+$db = new SQLite3('game-results.db');
+echo "!";
+$result = $db->query('SELECT * FROM results');
+echo "~";
+
+echo "<table>";
+echo "<tr><th>Target</th><th>Solution</th></tr>";
+while ($row = $result->fetchArray()) {
+    
+	echo "<tr>";
+	echo "<td>{$row["target"]}</td>";
+	echo "<td>{$row["solution"]}</td>";
+	echo "</tr>";
+}
+echo "</table>";
+echo "#";
+?>
+
+</body>
+</html>
